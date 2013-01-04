8ded75290cf60e4ed5da30406105e7549ce09735
hkpeprah <HayfordJunior@hotmail.com>
2013-01-03 23:01:13 -0500

    Changes to design, cleaned up redundant code, series to be implemented.

--- torrentfetch.py
+++ torrentfetch.py
@@ -14,9 +14,9 @@
 import os, sys, re, optparse
 from torrentfeed import *
 
-ver = "0.1.0"
 contributers = [] #List of contributors
-
+home = os.path.expanduser("~")
+directory = home + "/torrentfetch"
 
 def fetchTorrent( ):
     """Fetches the torrent with the given string by using the torrent sources.
@@ -24,7 +24,7 @@ def fetchTorrent( ):
     s = raw_input("Enter torrent name:\n> ")
     string = addTorrent(s)
     if not( string == None ):
-        with open("download_torrents.txt", "a") as myfile:
+        with open(directory + "/download_torrents.txt", "a") as myfile:
             myfile.write(string + "\n")
 
 
@@ -36,25 +36,29 @@ def fetchSeries( ):
 
 
 def downloads( ):
-    """Lists the latest torrent downloads that the user has completed.  If no number is specified it
-       defaults at 10."""
-    print "Listing last 10 downloads:"
+    """Lists the latest torrent downloads."""
     try:
-        with open("download_torrents.txt", "r") as myfile:
-            index = 0
+        print "Listing latest downloads:"
+        with open(directory + "/download_torrents.txt", "r") as myfile:
             for line in myfile:
-                if index > 9: break;
                 print line
-                index += 1
     except:
+        dfile = open(directory + "/download_torrents.txt", "w+")
+        dfile.write("\n")
+        dfile.close()
         print "No downloads as of yet."
-
 
 
 def main():
     """Function that accepts the main input that branches the script.  The user will select options based on what
        what they wish to do.  Depending on the input, the program will switch to the appropriate branch and receive
        and or provide input."""
+    #Ensure directory exists, otherwise create it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        dfile = open(directory + "/download_torrents.txt", 'w+')
+        dfile.write("\n")
+        dfile.close()
     while True:
         print "Enter the corresponding number: \n0. Download new torrent.\n1. Schedule torrent or connect to series (RSS).\n2. List latest downloads.",
         print "\n3. View current downloads.\n4. View scheduled torrents and series.\n5. Exit"
@@ -68,8 +72,6 @@ def main():
                 fetchSeries()
                 if (not( raw_input("Schedule another torrent, 'y' or 'n'?\n> ") == "y")): break
         elif branch == 2: downloads()
-        elif branch == 3: print "Current downloads"
-        elif branch == 4: print "Scheduled torrents"
         else: break
 
 
@@ -77,22 +79,16 @@ if __name__ == "__main__":
     """Main execution of the script, simply provides information about the contributers, author and the current version.
        Provides some information about the program then executes the main function for branching.  Exits on end of the
        main program."""
-    parser = optparse.OptionParser("usage: %prog [ options ]")
+    parser = optparse.OptionParser(usage="usage: %prog [ options ]", version="%prog v0.1.0")
     parser.add_option("-t", "--torrent", dest="torrent", help="enter the torrent name to search for")
     parser.add_option("-s", "--series", dest="series", help="enter the series name to add and fetch episodes")
-    parser.add_option("-v", "--version", dest="version", default=False, help="the program's version", action='store_true')
-    parser.add_option("-d", "--downloads", dest="downloads", default=False, help="recent ten downloads", action='store_true')
-    parser.add_option("-l", "--list", dest="list_series", default=False, help="list all the series that are added", action='store_true')
+    parser.add_option("-d", "--downloads", dest="downloads", default=False, help="recent downloads", action='store_true')
     (options, args) = parser.parse_args()
     if ( len(sys.argv) == 1 ):
-        print "Torrent Feeder Script\nVersion: " + ver + "\nauthor: hkpeprah\nContributors: "
-        for person in range(0, len(contributers)): print contributors[person] + " "
-        print "This script acts as a tool for enabling the user to download torrents\nand stay connected to recurring torrents.\n"
         main()
     elif ( sys.argv > 1) :
         #Parse the command-line arguments
-        if ( options.version == True ): print "Version: " + str(ver)
-        elif ( options.downloads == True  ): downloads()
+        if ( options.downloads == True  ): downloads()
         elif ( not( options.torrent == None ) and options.series == None ): fetchTorrent( str(torrent) )
         elif ( not( options.series == None ) ): fetchSeries( str(series) )
         else: print parser.usage
